c5266196b48f5cf125e9d462de90f51f9e603908
Jildert Viet <jildertviet@MacBook-Pro-van-Jildert-21236.local>
2020-10-20 10:32:51 +0200

    Been a while, added physarum, and changed fbo use

---
+++ src/JPhysarum.cpp
@@ -0,0 +1,8 @@
+//
+//  JPhysarum.cpp
+//  visualizer
+//
+//  Created by Jildert Viet on 01-07-20.
+//
+
+#include "JPhysarum.hpp"
---
+++ src/JPhysarum.hpp
@@ -0,0 +1,13 @@
+//
+//  JPhysarum.hpp
+//  visualizer
+//
+//  Created by Jildert Viet on 01-07-20.
+//
+
+#ifndef JPhysarum_hpp
+#define JPhysarum_hpp
+
+#include <stdio.h>
+
+#endif /* JPhysarum_hpp */
